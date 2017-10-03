1f2f836d39dd69ad23c5c1465cc3f870d5bb6636
Dmitry Kossovich <d.kossovich@yadro.com>
2017-10-03 15:06:56 +0300

    implemented k-tree

--- main.py
+++ main.py
@@ -27,9 +27,9 @@ class ItemsCeeper:
         distFirstType, distSecondType = 0, 0
         for item in self.items:
             if item.type == 0:
-                distFirstType += math.sqrt((item.x - x)**2 + (item.y - y)**2)
+                distFirstType += math.sqrt((item.x - x) ** 2 + (item.y - y) ** 2)
             else:
-                distSecondType += math.sqrt((item.x - x)**2 + (item.y - y)**2)
+                distSecondType += math.sqrt((item.x - x) ** 2 + (item.y - y) ** 2)
         if distFirstType < distSecondType:
             return 0
         else:
@@ -50,10 +50,122 @@ class ItemsCeeper:
         pl.show()
 
 
+class kdTree:
+    class border:
+        def __init__(self, leftx, lefty, rightx, righty):
+            # left lower, right, upper
+            self.leftx, self.lefty, self.rightx, self.righty = leftx, lefty, rightx, righty
+            self.decision = -1
+
+        def contains(self, x, y):
+            return self.rightx >= x >= self.leftx and self.righty >= y >= self.lefty
+
+        def setDecision(self, decision):
+            self.decision = decision
+
+    class node:
+        def __init__(self, brd):
+            self.brd = brd
+            self.left = None
+            self.right = None
+            self.type = -1
+
+    def __init__(self, items, splitN):
+        self.items = items
+        eps = 0.00001
+        self.headNode = self.node(
+            self.border(
+                min(i.x for i in items) + eps, min(i.y for i in items) + eps,
+                max(i.x for i in items) + eps, max(i.y for i in items) + eps
+            ))
+        self.makeBoarding(False, self.headNode.brd, self.headNode, splitN)
+
+    # implements SAH heruistic
+    def doSAH(self, items, initBorder, isHorisonal):
+        splits = 20
+        step = 0
+        cur = 0
+        stop = 0
+        if isHorisonal:
+            step = math.abs(initBorder.rightx - initBorder.leftx) / splits
+            cur = min(initBorder.rightx, initBorder.leftx)
+            stop = max(initBorder.rightx, initBorder.leftx)
+        else:
+            step = math.abs(initBorder.righty - initBorder.lefty) / splits
+            cur = min(initBorder.righty, initBorder.lefty)
+            stop = max(initBorder.righty, initBorder.lefty)
+
+        #shows (dots from left side of line) - (dots from right side)
+        leftGreaterThenRight = []
+        while cur < stop:
+            if isHorisonal:
+                leftGreaterThenRight.append(
+                    sum(1 for item in self.items if item.x < cur and initBorder.contains(item.x, item.y)) -
+                    sum(1 for item in self.items if item.x >= cur and initBorder.contains(item.x, item.y))
+                )
+            else:
+                leftGreaterThenRight.append(
+                    sum(1 for item in self.items if item.y < cur and initBorder.contains(item.x, item.y)) -
+                    sum(1 for item in self.items if item.y >= cur and initBorder.contains(item.x, item.y))
+                )
+            cur += step
+
+        i = 0
+        minDif = 100000000.0
+        itemI = 0
+        for item in leftGreaterThenRight:
+            if item < minDif:
+                minDif = item
+                itemI = i
+            i += 1
+
+        if isHorisonal:
+            cur = min(initBorder.rightx, initBorder.leftx)
+            cur += step * itemI
+            return [self.border(initBorder.leftx, initBorder.lefty, initBorder.rightx, cur),
+                    self.border(initBorder.leftx, cur, initBorder.rightx, initBorder.righty)]
+        else:
+            cur = min(initBorder.righty, initBorder.lefty)
+            cur += step * itemI
+            return [self.border(initBorder.leftx, initBorder.lefty, cur, initBorder.righty),
+                    self.border(cur, initBorder.lefty, initBorder.rightx, initBorder.righty)]
+
+    def cntType(self, brd):
+        first, second = 0,0
+        for item in self.items:
+            if brd.contains(item.x, item.y):
+                if item.type == 0:
+                    first += 1
+                else:
+                    second += 1
+        if first > second:
+            return 0
+        else:
+            return 1
+
+    def makeBoarding(self, isHorisontal, initBorder, node, splitN):
+            newBorders = self.doSAH(self.items, initBorder, isHorisontal)
+            node.left, node.right = self.node(newBorders[0]), self.node(newBorders[1])
+            splitN -= 1
+            if splitN > 0:
+                self.makeBoarding(not isHorisontal, newBorders[0], node.left, splitN)
+                self.makeBoarding(not isHorisontal, newBorders[1], node.right, splitN)
+            else:
+                node.left.type, node.right.type = self.cntType(node.left), self.cntType(node.right)
+
+    #return only type of node
+    def search(self, x, y):
+        node = self.headNode
+        while node.type == -1:
+            if node.left.contains(x, y):
+                node = node.left
+            else:
+                node = node.right
+        return node.type
+
+
 ceeper = ItemsCeeper("data")
 ceeper.read()
 ceeper.draw()
 ceeper.addDot(0.111, -0.829)
 time.sleep(1000)
-
-
